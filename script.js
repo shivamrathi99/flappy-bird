f3afe6ad56981d9c7a7179196cb711287d197eb0
shivamrathi99 <rathishivam99@gmail.com>
2021-01-09 02:04:46 +0530

    styling change

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var canvas = document.querySelector("#mycanvas");
+var heightratio = 0.3;
+console.log("SS", canvas.width);
+canvas.style.height = `${canvas.width * heightratio}vh`;
+console.log(canvas.style.height);
 function toggleFullScreen() {
   if (!document.fullscreenElement) {
     document.documentElement.requestFullscreen();
